d34c65926afd4d727656b3867610c04cf0626697
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-09-17 17:09:45 +0700

    Add support `qwen/qwen3-next-80b-a3b-thinking`

--- ccproxy/constants.py
+++ ccproxy/constants.py
@@ -80,6 +80,7 @@ MODEL_INPUT_TOKEN_LIMIT: FrozenSet[Tuple[str, int]] = frozenset({
     ("qwen/qwen3-coder", 262_144),
     ("deepseek/deepseek-chat-v3.1", 163_840),
     ("x-ai/grok-code-fast-1", 256_000),
+    ("qwen/qwen3-next-80b-a3b-thinking", 262_144),
 })
 
 MODEL_INPUT_TOKEN_LIMIT_MAP: Dict[str, int] = dict(MODEL_INPUT_TOKEN_LIMIT)
@@ -97,6 +98,7 @@ MODEL_MAX_OUTPUT_TOKEN_LIMIT: FrozenSet[Tuple[str, int]] = frozenset({
     ("deepseek-chat", 8_192),
     ("qwen/qwen3-coder", 66_560),
     ("qwen/qwen3-coder-480b-a35b-07-25", 66_560),
+    ("qwen/qwen3-next-80b-a3b-thinking", 66_560),
     ("deepseek/deepseek-chat-v3.1", 134_144),
     ("x-ai/grok-code-fast-1", 10_000),
 })
